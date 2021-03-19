74aa8dea67638331cddb0113edb7474904170809
Demchyshyn <demchyshyn.v87@gmail.com>
2021-03-19 20:34:47 +0200

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,148 @@
+# dsfsdfsfsdfsdf
+
+'''
+sdfsdf
+sdfsdfsdf
+sdfsdfsdf
+'''
+"""
+sdfsdfsdfsdf
+"""
+
+# print(1,2,3,4, sep=' d ', end='!!!!')
+# print()
+
+# i = 3
+# f = 1.4
+# b = True  # False
+# n = None
+# i = True
+# print(type(i))
+# print(type(f))
+# print(type(b))
+# print(type(n))
+
+# a = b = c = 10
+# print(a)
+# print(b)
+# print(c)
+
+a = 2525
+b = 2525
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+
+# # int()
+# float(55)
+# # bool()
+# # str()
+# print(int('2'))
+# print(type(int('sss')))
+
+# '', [], {}, None, 0 # False
+
+# num = int(input('Введите число: '))
+# print(type(num))
+# print(num)
+#
+# l1 = [1, 2, 3, 4, 5, 6, 2]
+# l2 = [55, 66, 77]
+# print(l1[-2])
+# l1[1] = 66
+# l1[6] = 55
+# del l1[99]
+# print(len(l1))
+# l1.append(5)
+# l1.insert(1,66)
+# pop = l1.pop(88)
+# l1.remove(2)
+# l1.extend(l2)
+
+# print(l1)
+# print(l1.index(4))
+# l1.reverse()
+# print(l1)
+# l1.sort(reverse=True)
+# print(l1)
+# print(l1.count(66))
+# l1.clear()
+# l3 = l1
+# l3 = l1.copy()
+# l3[3] = 6666
+# print(l1)
+
+
+l1 = [1, 2, 3, 4, 5, 6, 2]
+
+# res = l1[-1:0]
+# print(res)
+
+# r = range(10)
+# print(r)
+# print(type(r))
+# l = list(r)
+# print(l)
+a = 8
+b = 9
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a is b)
+# print(a != b)
+# print(a is not b)
+
+# if a < b:
+#     print('hello')
+#     print('hello')
+# else:
+#     print('else')
+#
+# print()
+
+# num = int(input('Enter num:'))
+
+# print('yes') if num > 5 else print('no')
+
+
+# t = ("Max", 13, "BMW")
+# print(t.count("Max"))
+# t.index(13)
+
+dic = {
+    'name': 'Max',
+    'age': 13,
+}
+# print(dic['age2'])
+# print(dic.get('age2', 55))
+# del dic['name']
+# pop = dic.pop('age2', None)
+# popitem = dict([dic.popitem()])
+# print(popitem)
+# items = dic.items()
+# dic.setdefault('name2', 55)
+# print(dic)
+
+# i = 5
+# # i+=1
+# # i = i +1
+# while i > 8:
+#     print(i)
+#     i -= 1
+# else:
+#     print('Finish')
+
+# for i in ['a','b']:
+# for i in range(3,10,2):
+#     print(i)
+
+for k, v in dic.items():
+    print(k)
+    print(v)
